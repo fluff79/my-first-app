0ad15d3d59c8debc0ed6753aa87cee706e277566
Mark <mrm198@hotmail.com>
2018-04-30 21:35:18 +0100

    Modified templates to display posts from database.

--- freezer/views.py
+++ freezer/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Item
+
 
 # Create your views here.
 
 def post_list(request):
-    return render(request,'freezer/post_list.html', {})
+    posts = Item.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+    return render(request,'freezer/post_list.html', {'posts' : posts})
