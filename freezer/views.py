669f732e5f7c22b0264e82cf6fa8e29ebd5c1448
Mark <mrm198@hotmail.com>
2018-05-02 06:43:03 +0100

    Added view and template and CSS

--- freezer/views.py
+++ freezer/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Item
 
@@ -8,3 +8,7 @@ from .models import Item
 def post_list(request):
     posts = Item.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
     return render(request,'freezer/post_list.html', {'posts' : posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Item, pk = pk)
+    return render(request,'freezer/post_detail.html', {'post' : post})
